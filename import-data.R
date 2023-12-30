2a46c6db55882eae025bd93b8d72a35cb64d15e7
Federico Giovannetti <98782817+FedeGiovannetti@users.noreply.github.com>
2023-12-30 09:57:19 -0300

    Update import-data.R

--- import-data.R
+++ import-data.R
@@ -5,7 +5,3 @@ pubmed_query("((SES) OR (poverty)) AND (neuroscience)")
 pubmed_query("((SES) OR (poverty)) AND (brain)")
 
 
-d = read.csv(paste("SESNeuroscienceRefs/Data/dataset_pubmed_", "((SES) OR (poverty)) AND (brain)" , ".csv"))
-
-"dataset_pubmed_ ((SES) OR (poverty)) AND (brain) .csv"
-"dataset_pubmed_ ((SES) OR (poverty)) AND (brain) .csv"
